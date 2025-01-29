e8383d02c6b800094204cfac8122e0fea0891ecb
SvenMuth <sven1996.sm@gmail.com>
2025-01-30 00:08:59 +0100

    Defined size of tokens. Implemented functions to check neighbor token and started with translation of the tokens in a formula

--- main.c
+++ main.c
@@ -3,7 +3,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ERROR_PRINT(args ...)    fprintf(stderr, args)
+#define SIZE_TOKENS                 1000
+#define ERROR_PRINT(args ...)       fprintf(stderr, args)
+
+static int x = 0;
 
 typedef enum TYPE
 {
@@ -23,7 +26,7 @@ typedef enum TYPE
 
 typedef struct token
 {
-    char content;
+    char token_c;
     TYPE type;
 } token_t;
 
@@ -31,40 +34,113 @@ typedef struct token
 void generate_token(token_t* token, const TYPE type, const char content)
 {
     *token = (token_t) {
-        .content = content,
+        .token_c = content,
         .type = type,
     };
 }
 
 void print_tokens(const token_t* tokens)
 {
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < SIZE_TOKENS; i++)
     {
-        if (tokens[i].type == UNINITIALIZED)
+        if (tokens[i].type == UNINITIALIZED) break;
+
+        printf("content: %c     type: %d\n",
+            tokens[i].token_c, tokens[i].type);
+    }
+}
+
+bool check_left_token_mul_or_div(const token_t* tokens, const int index)
+{
+    TYPE type_left = UNINITIALIZED;
+    if (index - 1 >= 0)
+    {
+        type_left = tokens[index - 1].type;
+    }
+
+    if (type_left == MULTIPLICATION || type_left == DIVISION) return true;
+    return false;
+}
+
+bool check_right_token_mul_or_div(const token_t* tokens, const int index)
+{
+    TYPE type_right = UNINITIALIZED;
+    if (index + 1 < SIZE_TOKENS)
+    {
+        type_right = tokens[index + 1].type;
+    }
+
+    if (type_right == MULTIPLICATION || type_right == DIVISION) return true;
+    return false;
+}
+
+void translate_into_formula(const token_t* tokens)
+{
+    token_t* token_buffer = malloc(sizeof(token_t) * 10);
+    int token_buffer_index = 0;
+    bool parentheses = false;
+    for (int i = 0; i < SIZE_TOKENS; i++)
+    {
+        const char token_c = tokens[i].token_c;
+        const TYPE type = tokens[i].type;
+
+        if (type == PARENTHESE_CLOSE)
         {
+            parentheses = false;
             break;
         }
 
-        printf("content: %c     type: %d\n", tokens[i].content, tokens[i].type);
-    }
+        if (parentheses)
+        {
+            token_buffer[token_buffer_index] = tokens[i];
+            token_buffer_index++;
+        }
 
+        if (type == UNINITIALIZED) break;
+        if (type == PARENTHESE_OPEN)
+        {
+            parentheses = true;
+            continue;
+        }
+
+        if (type == ADDITION || type == SUBTRACTION)
+        {
+            if (check_left_token_mul_or_div(tokens, i))
+            {
+
+            }
+            if (check_right_token_mul_or_div(tokens, i))
+            {
+
+            }
+        }
+        if (type == MULTIPLICATION)
+        {
+
+        }
+        if (type == DIVISION)
+        {
+
+        }
+    }
 }
+
 
 int main(void)
 {
-    const char* integral = "x ^ 3 + 2 x ^ 2 - 4 x + 5";
+    const char* integral = "X ^ 3 + 2 x ^ 2 - 4 x + 5";
 
-    token_t* tokens = malloc(sizeof(token_t) * 1000);
+    token_t* tokens = malloc(sizeof(token_t) * SIZE_TOKENS);
     if (tokens == nullptr)
     {
         ERROR_PRINT("Memory allocation failed!");
         return EXIT_FAILURE;
     }
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < SIZE_TOKENS; i++)
     {
         tokens[i] = (token_t){
-            .content = '0',
+            .token_c = '0',
             .type = UNINITIALIZED
         };
     }
@@ -74,53 +150,30 @@ int main(void)
 
     for (int i = 0; i < strlen(integral); i++)
     {
-        const char c = integral[i];
-        if (isspace(c) || iscntrl(c))
-        {
-            continue;
-        }
+        char c = integral[i];
+        if (isspace(c) || iscntrl(c)) continue;
 
-        if (isdigit(c))
-        {
-            type = NUMBER;
-        }
+        if (isdigit(c)) type = NUMBER;
+
         if (isalpha(c))
         {
             type = VARIABLE;
+            if (isupper(c))
+            {
+                c = (char)tolower(c);
+            }
         }
 
         switch (c)
         {
-        case MULTIPLICATION:
-            type = MULTIPLICATION;
-            break;
-
-        case DIVISION:
-            type = DIVISION;
-            break;
-
-        case ADDITION:
-            type = ADDITION;
-            break;
-
-        case SUBTRACTION:
-            type = SUBTRACTION;
-            break;
-
-        case POWER:
-            type = POWER;
-            break;
-
-        case PARENTHESE_OPEN:
-            type = PARENTHESE_OPEN;
-            break;
-
-        case PARENTHESE_CLOSE:
-            type = PARENTHESE_CLOSE;
-            break;
-
-        default:
-            break;
+        case MULTIPLICATION: type = MULTIPLICATION; break;
+        case DIVISION: type = DIVISION; break;
+        case ADDITION: type = ADDITION; break;
+        case SUBTRACTION: type = SUBTRACTION; break;
+        case POWER: type = POWER; break;
+        case PARENTHESE_OPEN: type = PARENTHESE_OPEN; break;
+        case PARENTHESE_CLOSE: type = PARENTHESE_CLOSE; break;
+        default: break;
         }
 
         generate_token(&tokens[token_index], type, c);
@@ -128,6 +181,7 @@ int main(void)
     }
 
     print_tokens(tokens);
+    translate_into_formula(tokens);
 
     free(tokens);
     return EXIT_SUCCESS;
